07635e576f7b97f6f1dc4a844bbdaa4a2b777ca3
Yazhlhub <yazhini23042000@gmail.com>
2021-07-15 19:00:21 +0530

    First commit

---
+++ WebApplication1/Controllers/anim2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class anim2Controller : Controller
+    {
+        // GET: anim2
+        public ActionResult anim2()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/animation1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class animation1Controller : Controller
+    {
+        // GET: animation1
+        public ActionResult animation1()
+        {
+            return View();
+        }
+    }
+}
